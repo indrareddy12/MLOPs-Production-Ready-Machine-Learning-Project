774872dbe526e8af8a34d5be09180524a32c5dd7
indrareddy12 <indrasenareddybathini86@gmail.com>
2025-10-18 08:31:46 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="indrasenareddy",
+    author_email="indrasenareddyabthini863@gmail.com",
+    packages=find_packages()
+)
